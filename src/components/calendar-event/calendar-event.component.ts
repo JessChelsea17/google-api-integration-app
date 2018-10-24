3b72a4c7e4298ab763b4133d8b0ce33c7915aef3
Jessica Carolina Rodriguez <malicdemj@malicdemj.mnl.corp.google.com>
2018-10-24 18:17:23 +0800

    working calendar

--- src/components/calendar-event/calendar-event.component.ts
+++ src/components/calendar-event/calendar-event.component.ts
@@ -18,50 +18,35 @@ export class CalendarEventComponent implements OnInit {
     private zone: NgZone,
   ) { }
 
-  ngOnInit() {
-    this.test();
-  }
-
-  test() {
-    var reqBody = {
-      start: {
-        dateTime: '2018-10-24T14:30:00-07:00',
-        timeZone: 'America/Los_Angeles'
-      },
-      end: {
-        dateTime: '2018-10-24T14:30:00-07:00',
-        timeZone: 'America/Los_Angeles'
-      }
-    }
-    gapi.client['calendar'].events.insert({
-      calendarId: 'primary',
-      resource:  reqBody
-    }).then((res) => {
-          this.zone.run(() => {
-            console.log('REQUEST: ', res);
-          });
-    });
-}
+  ngOnInit() {}
 
   addEvent(eventname, location, description, startdate, enddate, timezone) {
     if(!eventname || !startdate || !enddate || !timezone) {
-      console.log(eventname)
-      console.log(startdate)
-      console.log(enddate)
-      console.log(timezone)
       alert('Please fill in all required fields');
     } else {
       startdate = startdate + ':00';
       enddate = enddate + ':00';
-      console.log(eventname)
-      console.log(location)
-      console.log(description)
-      console.log(startdate)
-      console.log(enddate)
-      // console.log(timezone)
-      // this.eventService.saveEvent({eventname, location, description, startdate, enddate, timezone} as Event).subscribe(event => {
-      //   this.newEvent.emit(event);
-      // });
+      var reqBody = {
+        'summary': eventname,
+        'location': location,
+        'description': description,
+        'start': {
+          'dateTime': startdate,
+          'timeZone': timezone
+        },
+        'end': {
+          'dateTime': startdate,
+          'timeZone': timezone
+        }
+      }
+      gapi.client['calendar'].events.insert({
+        calendarId: 'primary',
+        resource:  reqBody
+      }).then((res) => {
+            this.zone.run(() => {
+              console.log('REQUEST: ', res);
+            });
+      });
     }
   }
 
