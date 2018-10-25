03a430d7988f1563c6e3a222fe5e97a9d5ccb6ef
Jessica Carolina Rodriguez <malicdemj@malicdemj.mnl.corp.google.com>
2018-10-25 17:02:38 +0800

    - download bootstrap - dropdown default selector -  delete event.service - add property binding in src iframe

--- src/components/calendar-event/calendar-event.component.ts
+++ src/components/calendar-event/calendar-event.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, EventEmitter, Output, Input, NgZone } from '@angular/core';
 import { Event } from './Event';
 import { Router } from '@angular/router';
+import { User } from "../../models/user";
+import { AppContext } from "../../infrastructure/app.context";
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-calendar-event',
@@ -12,13 +15,23 @@ export class CalendarEventComponent implements OnInit {
   @Output() newEvent: EventEmitter<Event> = new EventEmitter();
   @Input() currentEvent: Event;
   @Input() isEdit: boolean;
+  users: User[]=[];
+  url: string = `https://calendar.google.com/calendar/embed?showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=malicdemj@google.com&amp;color=%23AB8B00&amp;ctz=Asia%2FManila`;
+  urlSafe: SafeResourceUrl;
+  selectedUserEmail: string;
 
   constructor(
     private router: Router,
     private zone: NgZone,
+    private appContext: AppContext,
+    private sanitizer: DomSanitizer
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.users = this.appContext.Repository.User.getAll();
+    this.selectedUserEmail = this.users[1].Email;
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  }
 
   addEvent(eventname, location, description, startdate, enddate, timezone) {
     if(!eventname || !startdate || !enddate || !timezone) {
