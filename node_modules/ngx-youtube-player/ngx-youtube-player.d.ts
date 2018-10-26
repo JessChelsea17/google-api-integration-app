ab83f198b6cd6b38dccae59b59b50cc5bfdb5b4f
Jessica Carolina Rodriguez <malicdemj@malicdemj.mnl.corp.google.com>
2018-10-26 10:44:49 +0800

    add youtube modules

---
+++ node_modules/ngx-youtube-player/ngx-youtube-player.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { YoutubePlayerComponent as ɵa } from './src/modules/youtube-player.component';
+export { YoutubePlayerService as ɵb } from './src/services/youtube-player.service';
